8c3eab3e1ebc6dd229ce0def4569961a07a723e4
Navneet Verma <navneev@amazon.com>
2024-05-02 07:25:13 +0000

    Fixed query benchmarks to run single query at a time
    Signed-off-by: Navneet Verma <navneev@amazon.com>

--- python/search/search_indices.py
+++ python/search/search_indices.py
@@ -7,6 +7,8 @@ import logging
 from timeit import default_timer as timer
 from python.decorators.timer import timer_func
 from python.utils.common_utils import recall_at_r
+import numpy as np
+from tqdm import tqdm
 
 def runIndicesSearch(xq, graphFile:str, param:dict, gt) -> dict:
     index:faiss.Index = loadGraphFromFile(graphFile)
@@ -14,14 +16,18 @@ def runIndicesSearch(xq, graphFile:str, param:dict, gt) -> dict:
     hnswParameters.efSearch = 100 if param.get('ef_search') is None else param['ef_search']
     k = 100 if param.get('K') is None else param['K']
     
-    @timer_func
     def search(xq, k, params):
         D, ids = index.search(xq, k, params=params)
         return ids
     # K is always set to 100
-    t1 = timer()
-    I = search(xq, 100, hnswParameters)
-    t2 = timer()
+    total_time = 0
+    I = []
+    for query in tqdm(xq, total=len(xq), desc=f"Running queries for ef_search: {hnswParameters.efSearch}"):
+        t1 = timer()
+        result = search(np.array([query]), 100, hnswParameters)
+        t2 = timer()
+        I.append(result[0])
+        total_time = total_time + (t2-t1)
 
     recall_at_k = recall_at_r(I, gt, k, k, len(xq))
     recall_at_1 = recall_at_r(I, gt, 1, 1, len(xq))
@@ -31,12 +37,12 @@ def runIndicesSearch(xq, graphFile:str, param:dict, gt) -> dict:
     # We don't need to set own_fileds = true as this will be automatically set by faiss while reading the index.
     del index
     return {
-        "searchTime": t2 - t1,
+        "searchTime": total_time,
         "units": "seconds",
         f"recall_at_{k}": recall_at_k,
         "recall_at_1": recall_at_1,
         "total_queries": len(xq),
-        "search_throughput" : len(xq)/( t2 - t1)
+        "search_throughput" : len(xq)/total_time
     }
 
 
