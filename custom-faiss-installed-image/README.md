ba54dc82bbff03576af9660ac239fa954275869c
Navneet Verma <navneev@amazon.com>
2024-12-27 21:32:25 +0000

    Code for creating the nvidia based docker image and custom faiss image
    Signed-off-by: Navneet Verma <navneev@amazon.com>

---
+++ custom-faiss-installed-image/README.md
@@ -0,0 +1,61 @@
+## Creating the image
+Below are the steps to create the image that can have a custom faiss python code build.
+1. The basic docker file is present in this folder named Dockerfile.
+2. Ensure that faiss is checked out before you trigger the build. The commit of faiss I have tested with is: 3c8dc4194907e9b911551d5a009468106f8b9c7f
+3. Build the docker image using this command
+```
+docker build -t custom-faiss . --no-cache
+```
+4. Once image is build use the below command to run the container.
+```
+docker run --gpus all -d -v ./:/tmp/ custom-faiss:latest
+```
+Make sure to run the command from the same folder where this readme is present otherwise wrong files get copied.
+5. Now ssh into the container
+```
+docker container exec -it <container-id>  /bin/bash
+```
+6. Once you ssh in the container make sure that you are sshed as `appuser`.
+7. Now go to `tmp/faiss` using `cd /tmp/faiss`
+8. Now run below command to trigger the cmake build.
+```
+cmake -B build \
+    -DBUILD_SHARED_LIBS=ON \
+    -DFAISS_ENABLE_GPU=ON \
+    -DFAISS_OPT_LEVEL=generic \
+    -DFAISS_ENABLE_C_API=ON \
+    -DFAISS_ENABLE_PYTHON=ON \
+    -DPYTHON_EXECUTABLE=$CONDA/bin/python \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CUDA_ARCHITECTURES="${CUDA_ARCHS}" \
+    -DFAISS_ENABLE_CUVS=ON \
+    -DCUDAToolkit_ROOT="/usr/local/cuda/lib64" \
+    .
+```
+9. Now run the below command to make faiss. Here I am using 4 cores to build faiss, you can use more if you have more cores. Just make sure that you are not using all cores otherwise the machine will get struck. This command takes a lot of time to run and build faiss
+```
+make -C build -j6 faiss swigfaiss
+```
+10. Once the build is complete now run the below command to create faiss bindings.
+```
+cd build/faiss/python && python3 setup.py build
+```
+11. One possible way to use faiss which you have built is like this, that can be added in the code.
+```
+import sys
+sys.path.append('/tmp/faiss/build/faiss/python')
+import faiss
+```
+Another approach can be setting the PYTHONPATH variable like this
+```
+export PYTHONPATH="$(ls -d `pwd`/tmp/faiss/build/faiss/python/build/lib*/):`pwd`/"
+```
+12. Now to test if everthing is build correctly or not run the below command
+```
+python faiss-test.py
+```
+and see an output like this
+```
+Creating GPU Index.. with IVF_PQ
+Indexing done
+```
--- testing_nvidia_docker/faiss-test.py
+++ testing_nvidia_docker/faiss-test.py
@@ -1,12 +1,17 @@
+import sys
+#sys.path.append('/tmp/faiss/build/faiss/python')
 import faiss
 import logging
 from timeit import default_timer as timer
 import math
 import numpy as np
+import time
+
+from numpy import dtype
 
 
 def indexData(d:int, xb:np.ndarray, ids:np.ndarray, indexingParams:dict, space_type:str, file_to_write:str="gpuIndex.cagra.graph"):
-    num_of_parallel_threads = 4
+    num_of_parallel_threads = 8
     logging.info(f"Setting number of parallel threads for graph build: {num_of_parallel_threads}")
     faiss.omp_set_num_threads(num_of_parallel_threads)
     res = faiss.StandardGpuResources()
@@ -17,7 +22,8 @@ def indexData(d:int, xb:np.ndarray, ids:np.ndarray, indexingParams:dict, space_t
     cagraIndexConfig.intermediate_graph_degree = 64 if indexingParams.get('intermediate_graph_degree') is None else indexingParams['intermediate_graph_degree']
     cagraIndexConfig.graph_degree = 32 if indexingParams.get('graph_degree') == None else indexingParams['graph_degree']
     cagraIndexConfig.device = faiss.get_num_gpus() - 1
-    cagraIndexConfig.store_dataset = False
+    #cagraIndexConfig.conservative_memory_allocation = True
+    #cagraIndexConfig.store_dataset = False
 
     cagraIndexConfig.build_algo = faiss.graph_build_algo_IVF_PQ
     cagraIndexIVFPQConfig = faiss.IVFPQBuildCagraConfig()
@@ -26,6 +32,7 @@ def indexData(d:int, xb:np.ndarray, ids:np.ndarray, indexingParams:dict, space_t
     cagraIndexIVFPQConfig.pq_dim = 32 if indexingParams.get('pq_dim') == None else indexingParams['pq_dim']
     cagraIndexIVFPQConfig.n_lists = int(math.sqrt(len(xb))) if indexingParams.get('n_lists') == None else indexingParams['n_lists']
     cagraIndexIVFPQConfig.kmeans_trainset_fraction = 10 if indexingParams.get('kmeans_trainset_fraction') == None else indexingParams['kmeans_trainset_fraction']
+    cagraIndexIVFPQConfig.conservative_memory_allocation = True
     cagraIndexConfig.ivf_pq_params = cagraIndexIVFPQConfig
 
     cagraIndexSearchIVFPQConfig = faiss.IVFPQSearchCagraConfig()
@@ -33,12 +40,12 @@ def indexData(d:int, xb:np.ndarray, ids:np.ndarray, indexingParams:dict, space_t
     cagraIndexConfig.ivf_pq_search_params = cagraIndexSearchIVFPQConfig
 
     print("Creating GPU Index.. with IVF_PQ")
+    t1 = timer()
     cagraIVFPQIndex = faiss.GpuIndexCagra(res, d, metric, cagraIndexConfig)
     idMapIVFPQIndex = faiss.IndexIDMap(cagraIVFPQIndex)
     indexDataInIndex(idMapIVFPQIndex, ids, xb)
-    
-    
-
+    t2 = timer()
+    print(f"Indexing time: {t2 - t1} sec")
 
 def indexDataInIndex(index: faiss.Index, ids, xb):
     if ids is None:
@@ -46,9 +53,35 @@ def indexDataInIndex(index: faiss.Index, ids, xb):
     else:
         index.add_with_ids(xb, ids)
 
-if __name__ == "__main__":    
-    d = 128
+if __name__ == "__main__":
+    d = 768
     np.random.seed(1234)
-    xb = np.random.random((100000, d)).astype('float32')
-    ids = [i for i in range(len(xb))]
+    # Create a new memory-mapped array
+    shape = (4_000_000, 768)  # Example shape
+    filename = 'mmap.npy'
+
+    # mmap_array = np.lib.format.open_memmap(filename, mode='w+', dtype='float32', shape=shape)
+    # mmap_array[:] = np.random.rand(*shape)
+    # del mmap_array
+    print("file is written")
+
+    #xb = np.lib.format.open_memmap(filename, mode='r')
+    xb = np.load(file = filename)
+    #xb2 = np.load(file=filename)
+    #xb = np.vstack((xb, xb2))
+
+    # array1 = np.load(file = filename)
+    # array2 = np.load(file = filename)
+    # xb = np.vstack((array1, array2))
+    print(xb.shape)
+
+    # Save changes to disk
+    #xb = np.load('my_array.dat', allow_pickle=True)
+    #xb = np.random.random((4_000_000, d)).astype('float32')
+    #ids = [i for i in range(len(xb))]
+    ids = None
     indexData(d, xb, ids, {}, "l2", "testgpuIndex.cagra.graph")
+    print("Indexing done")
+    del xb
+    time.sleep(10)
+    print("Deleted xb")
