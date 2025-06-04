e93651978eeedd47d6c0255671ca07caaacc536b
Rohan Chitale <rchital@amazon.com>
2025-06-03 18:36:02 -0700

    Re-factor benchmarking tool to work with production remote vector core code (#8)
    Signed-off-by: Rohan Chitale <rchital@amazon.com>

--- benchmarking/workload/workload.py
+++ benchmarking/workload/workload.py
@@ -6,16 +6,14 @@ from typing import List
 
 from benchmarking.data_types.data_types import IndexTypes, WorkloadTypes
 from benchmarking.dataset import dataset_utils
-from benchmarking.memory_profiler.gpu_memory_profiler import GPUMemoryMonitor
+from benchmarking.memory_profiler.memory_monitor import MemoryMonitor
 from benchmarking.search import search_indices
 from benchmarking.utils.common_utils import ensureDir
 import json
 import time
 from tqdm import tqdm
+import copy
 
-from core.index_builder.index_builder_utils import (
-    calculate_ivf_pq_n_lists,
-)
 
 from core.common.models import VectorsDataset, SpaceType
 from benchmarking.service.faiss_index_build_service import FaissIndexBuildService
@@ -68,7 +66,7 @@ def executeWorkload(
         or workloadType == WorkloadTypes.INDEX
     ):
         indexingMetrics = doIndexing(
-            workloadToExecute, dataset_file, indexType, workloadType
+            workloadToExecute, dataset_file, indexType
         )
         allMetrics[workloadName] = {
             "workload-details": indexingMetrics["workload-details"],
@@ -80,7 +78,7 @@ def executeWorkload(
         or workloadType == WorkloadTypes.SEARCH
     ):
         searchMetrics = doSearch(
-            workloadToExecute, dataset_file, indexType, workloadType
+            workloadToExecute, dataset_file, indexType
         )
         allMetrics[workloadName]["searchMetrics"] = searchMetrics["search-metrics"]
         allMetrics[workloadName]["workload-details"] = searchMetrics["workload-details"]
@@ -103,8 +101,7 @@ def persistMetricsAsJson(
 def doIndexing(
     workloadToExecute: dict,
     datasetFile: str,
-    indexType: IndexTypes,
-    workloadType: WorkloadTypes,
+    indexType: IndexTypes
 ):
     logging.info("Run Indexing...")
     d, xb, ids = dataset_utils.prepare_indexing_dataset(
@@ -113,81 +110,30 @@ def doIndexing(
         workloadToExecute.get("indexing-docs"),
     )
 
-    vectors_dataset = VectorsDataset(xb, ids)
-
     workloadToExecute["dimension"] = d
     workloadToExecute["vectorsCount"] = len(xb)
 
-    space_type = (
-        SpaceType("l2")
-        if workloadToExecute.get("space-type") is None
-        else SpaceType(workloadToExecute.get("space-type"))
-    )
-
     parameters_level_metrics = []
     for param in tqdm(workloadToExecute["indexing-parameters"]):
-        prepare_env_for_indexing(workloadToExecute, indexType, param)
-        timingMetrics = None
-        metrics = {"indexing-param": param}
-        logging.info(
-            f"================ Running configuration: {param} ================"
-        )
-        if indexType == IndexTypes.CPU:
-            from benchmarking.indexing.cpu.create_cpu_index import (
-                indexData as indexDataInCpu,
-            )
-
-            timingMetrics = indexDataInCpu(
-                d, xb, ids, param, space_type, file_to_write=param["graph_file"]
-            )
-
+        # TODO: Include compression in indexing-parameters field in yaml
+        # If compression is in indexing-parameters field, then we don't need this if statement and inner for loop
         if indexType == IndexTypes.GPU:
-            # from benchmarking.indexing.gpu.create_gpu_index import (indexData as indexDataInGpu,)
+            for compression in workloadToExecute['compression']:
+                param = copy.deepcopy(param)
+                compression = int(compression)
+                if compression != 0:
+                    param["ivf_pq_params"]["pq_dim"] = int(workloadToExecute["dimension"] / compression)
+                else:
+                    param["ivf_pq_params"]["pq_dim"] = 0
+                metrics = get_indexing_metrics(workloadToExecute, indexType, param, xb, ids)
+                parameters_level_metrics.append(metrics)
+        else:
+            metrics = get_indexing_metrics(workloadToExecute, indexType, param, xb, ids)
+            parameters_level_metrics.append(metrics)
 
-            monitor = GPUMemoryMonitor()
-
-            try:
-                monitor.start_monitoring()
-                param["ivf_pq_build_params"]["n_lists"] = calculate_ivf_pq_n_lists(
-                    len(xb)
-                )
-                param["ivf_pq_build_params"]["force_random_rotation"] = True
-
-                faiss_index_build_service = FaissIndexBuildService()
-                timingMetrics = faiss_index_build_service.build_index(
-                    param,
-                    workloadToExecute["search-parameters"][0],
-                    vectors_dataset,
-                    workloadToExecute,
-                    param["graph_file"],
-                )
-                metrics["indexing-timingMetrics"] = timingMetrics
-                # timingMetrics = indexDataInGpu(d, xb, ids, param, space_type, param["graph_file"])
-                time.sleep(3)
-
-            finally:
-                monitor.stop_monitoring()
-                logging.info(json.dumps(monitor.memory_logs))
-                logging.info(json.dumps(monitor.ram_used_mb))
-                monitor.log_metrics()
-                metrics["memory_metrics"] = {
-                    "timestamps": monitor.timestamps,
-                    "gpu_memory_logs": monitor.memory_logs,
-                    "start_time": monitor.start_time,
-                    "gpu_id": monitor.gpu_id,
-                    "ram_used_kb": monitor.ram_used_mb,
-                    "interval": monitor.interval
-                }
-
-        logging.info(f"===== Timing Metrics : {timingMetrics} ====")
-        logging.info(
-            f"================ Completed configuration: {param} ================"
-        )
-        parameters_level_metrics.append(metrics)
         logging.info("Sleeping for 5 sec for better metrics capturing")
         time.sleep(5)
 
-    del vectors_dataset
     del xb
     del ids
     return {
@@ -196,11 +142,11 @@ def doIndexing(
     }
 
 
+
 def doSearch(
     workloadToExecute: dict,
     datasetFile: str,
-    indexType: IndexTypes,
-    workloadType: WorkloadTypes,
+    indexType: IndexTypes
 ):
     logging.info("Running Search...")
     d, xq, gt = dataset_utils.prepare_search_dataset(
@@ -210,50 +156,27 @@ def doSearch(
     workloadToExecute["queriesCount"] = len(xq)
     parameters_level_metrics = []
     for indexingParam in workloadToExecute["indexing-parameters"]:
-        if workloadType in (WorkloadTypes.SEARCH, WorkloadTypes.INDEX_AND_SEARCH):
-            dir_path = ensureDir("graphs")
-            put_graph_file_name_in_param(
-                workloadToExecute, d, indexType, indexingParam, dir_path
-            )
-        for searchParam in workloadToExecute["search-parameters"]:
-            logging.info(
-                f"=== Running search for index config: {indexingParam} and search config: {searchParam}==="
-            )
-            searchTimingMetrics = search_indices.runIndicesSearch(
-                xq, indexingParam["graph_file"], searchParam, gt
-            )
-            logging.info(f"===== Timing Metrics : {searchTimingMetrics} ====")
-            logging.info(
-                f"=== Completed search for index config: {indexingParam} and search config: {searchParam}==="
-            )
-            logging.info(f"=======")
-            parameters_level_metrics.append(
-                {
-                    "indexing-params": indexingParam,
-                    "search-timing-metrics": searchTimingMetrics,
-                    "search-params": searchParam,
-                }
-            )
-            logging.info("Sleeping for 5 sec for better metrics capturing")
-            time.sleep(5)
+        # TODO: Include compression in indexing-parameters field in yaml
+        # If compression is in indexing-parameters field, then we don't need this if statement and inner for loop
+        if indexType == IndexTypes.GPU:
+            for compression in workloadToExecute['compression']:
+                indexingParam = copy.deepcopy(indexingParam)
+                if compression != 0:
+                    indexingParam["ivf_pq_params"]["pq_dim"] = int(workloadToExecute["dimension"] / compression)
+                else:
+                    indexingParam["ivf_pq_params"]["pq_dim"] = 0
+                metrics = get_search_metrics(workloadToExecute, indexType, indexingParam, xq, gt)
+                parameters_level_metrics = parameters_level_metrics + metrics
+        else:
+            metrics = get_search_metrics(workloadToExecute, indexType, indexingParam, xq, gt)
+            parameters_level_metrics = parameters_level_metrics + metrics
+
     del xq
     del gt
     return {
         "workload-details": workloadToExecute,
         "search-metrics": parameters_level_metrics,
     }
-
-
-def prepare_env_for_indexing(
-    workloadToExecute: dict, indexType: IndexTypes, param: dict
-):
-    dir_path = ensureDir("graphs")
-    d = workloadToExecute["dimension"]
-    put_graph_file_name_in_param(workloadToExecute, d, indexType, param, dir_path)
-    if os.path.exists(param["graph_file"]):
-        logging.info(f"Removing file : {param['graph_file']}")
-        os.remove(param["graph_file"])
-
 
 def readAllWorkloads():
     with open("/benchmarking/benchmarks.yml") as stream:
@@ -264,12 +187,109 @@ def readAllWorkloads():
             sys.exit()
 
 
-def put_graph_file_name_in_param(
-    workloadToExecute: dict, d: int, indexType: IndexTypes, param: dict, dir_path: str
-):
+def get_graph_file(workloadToExecute: dict, indexType: IndexTypes, param: dict):
+    dir_path = ensureDir("graphs")
+    d = workloadToExecute["dimension"]
     str_to_build = f"{workloadToExecute['dataset_name']}_{d}.{indexType.value}"
     sorted_param_keys = sorted(param.keys())
     for key in sorted_param_keys:
-        str_to_build += f"_{key}_{param[key]}"
+        value = str(param[key])
+        # replace special characters from param, to make graph name more readable
+        special_chars_to_remove = " {}',"
+        for character in special_chars_to_remove:
+            value = value.replace(character, "")
+        value = value.replace(":", "_")
+        str_to_build += f"_{key}_{value}"
     str_to_build += ".graph"
-    param["graph_file"] = os.path.join(dir_path, str_to_build)
+    return os.path.join(dir_path, str_to_build)
+
+def get_indexing_metrics(workloadToExecute, indexType, indexingParam, xb, ids):
+    graph_file = get_graph_file(workloadToExecute, indexType, indexingParam)
+    if os.path.exists(graph_file):
+        logging.info(f"Removing file : {graph_file}")
+        os.remove(graph_file)
+
+    metrics = {"indexing-param": indexingParam}
+    logging.info(
+        f"================ Running configuration: {indexingParam} ================"
+    )
+    monitor = None
+
+    try:
+        timingMetrics = None
+        if indexType == IndexTypes.GPU:
+            monitor = MemoryMonitor(graph_file, monitor_gpu=True)
+            monitor.start_monitoring()
+            vectors_dataset = VectorsDataset(xb, ids)
+            faiss_index_build_service = FaissIndexBuildService()
+            timingMetrics = faiss_index_build_service.build_index(
+                indexingParam,
+                workloadToExecute["search-parameters"][0],
+                vectors_dataset,
+                workloadToExecute,
+                graph_file,
+            )
+            del vectors_dataset
+        else:
+            monitor = MemoryMonitor(graph_file, monitor_gpu=False)
+            monitor.start_monitoring()
+            from benchmarking.indexing.cpu.create_cpu_index import (
+                indexData as indexDataInCpu,
+            )
+
+            space_type = (
+                SpaceType("l2")
+                if workloadToExecute.get("space-type") is None
+                else SpaceType(workloadToExecute.get("space-type"))
+            )
+
+            timingMetrics = indexDataInCpu(
+                workloadToExecute["dimension"], xb, ids, indexingParam, space_type, file_to_write=graph_file
+            )
+
+        metrics["indexing-timingMetrics"] = timingMetrics
+        logging.info(f"===== Timing Metrics : {timingMetrics} ====")
+        logging.info(
+            f"================ Completed configuration: {indexingParam} ================"
+        )
+    finally:
+        monitor.stop_monitoring()
+        logging.debug(json.dumps(monitor.gpu_memory_logs))
+        logging.debug(json.dumps(monitor.cpu_memory_logs))
+        logging.debug(json.dumps(monitor.ram_used_mb))
+        max_mem, start_mem, end_mem = monitor.log_gpu_metrics()
+        cpu_max, cpu_start, cpu_end = monitor.log_cpu_metrics()
+        metrics["memory_metrics"] = {
+            "peak_gpu_mem": max_mem - start_mem,
+            "peak_cpu_mem": cpu_max - cpu_start,
+        }
+
+    return metrics
+
+def get_search_metrics(workloadToExecute, indexType, indexingParam, xq, gt):
+    parameters_level_metrics = []
+    graph_file = get_graph_file(workloadToExecute, indexType, indexingParam)
+
+    for searchParam in workloadToExecute["search-parameters"]:
+        logging.info(
+            f"=== Running search for index config: {indexingParam} and search config: {searchParam}==="
+        )
+        searchTimingMetrics = search_indices.runIndicesSearch(
+            xq, graph_file, searchParam, gt
+        )
+        logging.info(f"===== Timing Metrics : {searchTimingMetrics} ====")
+        logging.info(
+            f"=== Completed search for index config: {indexingParam} and search config: {searchParam}==="
+        )
+        logging.info(f"=======")
+        parameters_level_metrics.append(
+            {
+                "indexing-params": indexingParam,
+                "search-timing-metrics": searchTimingMetrics,
+                "search-params": searchParam,
+            }
+        )
+        logging.info("Sleeping for 5 sec for better metrics capturing")
+        time.sleep(5)
+
+    return parameters_level_metrics
